e44aee8cdd3e1a5ac825d04bd50871618e5f01f6
Amir Yunas <ayunas@gmail.com>
2020-01-06 15:51:00 -0600

    set up Room and Player Models

--- pytower/settings.py
+++ pytower/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tower_app.apps.TowerAppConfig'
 ]
 
 MIDDLEWARE = [
--- tower_app/models.py
+++ tower_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    room = models.CharField(max_length = 64)
+
+    def __str__(self):
+        return f"{self.room}"
+
+class Player(models.Model):
+    name = models.CharField(max_length=64)
+    location = models.ForeignKey(Room, on_delete=models.CASCADE, blank=True, null=True)
+
+    def move(self):
+       pass
+    
+    def __str__(self):
+        # return f"{self.name}"
+        return f"{self.name} in {self.location}" 
+
+
+
+
