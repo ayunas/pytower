4721625708f4863107bad08a2721c802487a4e94
Shannon <shannon.yoshikawa@gmail.com>
2020-01-07 13:12:57 -0800

    added base, index, and play templates; created form for character creation

--- tower_app/views.py
+++ tower_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import CreateCharacter
+from .models import Player
 
 # Create your views here.
 def test(request):
     return HttpResponse('test is working properly')
 
 def index(request):
-    return HttpResponse('this is the index page view function')
+    if request.method == "POST":
+        form = CreateCharacter(request.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            character = Player(name=n)
+            character.save()
+        return HttpResponseRedirect("/play")
+    else:
+        form= CreateCharacter()
+
+    return render(request, "tower_app/index.html", {"form": form})
+
+def play(request):
+    player ={"name": "Player1", "location": "Foyer"}
+    return render(request, "tower_app/play.html", player)
