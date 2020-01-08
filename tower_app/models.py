80bc6f6023ecd318a9745d9c0ae2cf1fbd8108d5
Amir Yunas <ayunas@gmail.com>
2020-01-08 12:31:23 -0600

    modified the models to return strings instead of only printing out strings

--- tower_app/models.py
+++ tower_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import string,random
-import uuid
+# import uuid
 
 class Room(models.Model):
         ### Field Columns in Room Table ###
@@ -37,11 +37,19 @@ class Room(models.Model):
 
 class Player(models.Model):
     # uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+    HP = models.IntegerField(default=10)
     name = models.CharField(max_length=64, default=f"Room {random.choice(string.ascii_letters)}")#attempting to generate a random room name using ascii_letters from string library and random.choice()
-    room = models.ForeignKey(Room, on_delete=models.CASCADE, default=None, blank=True, null=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
+    # inventory = models.ForeignKey(Inventory)
 
-    def initialize(self):
-        start = input(f"{self.name}, you are outside the PyTower. It is a 10 story tower. There is a treasure chest on the top floor. Do you have what it takes to reach the top??? type 'y' to enter Pytower: ")
+    def pickup(self,item):
+        inv = Inventory(item = item)
+        print(inv)
+        inv.save()
+        # return Inventory.objects.get(item = item)
+
+    def initialize(self,start):
+        # start = input(f"{self.name}, you are outside the PyTower. It is a 10 story tower. There is a treasure chest on the top floor. Do you have what it takes to reach the top??? type 'y' to enter Pytower: ")
 
         if start == 'y':
             self.room = Room.objects.get(room_name = "Foyer")
@@ -62,7 +70,7 @@ class Player(models.Model):
         if way == 'up':
             if not self.room.up:
                 print('you cannot go that way. no rooms there...')
-                return
+                return 'you cannot go that way. no rooms there...'
             else:
                 self.room = Room.objects.get(room_name = self.room.up)
                 print('in room: ', self.room, 'up:',self.room.up, 'down:',self.room.down, 'left:',self.room.left, 'right:', self.room.right)
@@ -80,7 +88,7 @@ class Player(models.Model):
         elif way == 'left':
             if not self.room.left:
                 print('you cannot go that way. no rooms there...')
-                return
+                return 'you cannot go that way. no rooms there...')
             else:
                 self.room = Room.objects.get(room_name = self.room.left)
                 print('in room-', self.room, 'up-',self.room.up, 'down-',self.room.down, 'left-',self.room.left, 'right-', self.room.right)
@@ -89,7 +97,7 @@ class Player(models.Model):
         elif way == 'right':
             if not self.room.right:
                 print('you cannot go that way. no rooms there...')
-                return
+                return 'you cannot go that way. no rooms there...'
             else:
                 self.room = Room.objects.get(room_name = self.room.right)
                 print('in room: ', self.room, 'up:',self.room.up, 'down:',self.room.down, 'left:',self.room.left, 'right:', self.room.right)
@@ -97,7 +105,7 @@ class Player(models.Model):
             
         else:
             print('you have entered an invalid direction')
-            return
+            return 'you have entered an invalid direction'
 
     def __str__(self):
         if not self.room:
@@ -106,8 +114,24 @@ class Player(models.Model):
             return f"{self.name} in {self.room}"
 
 
+# class Inventory(models.Model):
+#     player = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
+#     item = models.CharField(max_length = 64)
+
+#     def __str__(self):
+#         return str(self.item)
+
 class Item(models.Model):
-    item_name = models.CharField(max_length=64, )
+    item_name = models.CharField(max_length=64)
+    strength = models.IntegerField(default=5)
+    item_type = models.CharField(max_length=64,default="weapon")
+    # playerID = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
+    # roomID = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
+    playerID = models.IntegerField(null=True)
+    roomID = models.IntegerField(default=1,null=True)
+
+    def __str__(self):
+        return f"{self.item_name} strength: {self.strength}"
 
 class Floor(models.Model):
     pass
